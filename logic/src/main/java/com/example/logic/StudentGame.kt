02a7b063f8d7058ded9aef38f707bc950d39e8dd
Charles <charlie_loves_skylines@hotmail.com>
2020-03-10 14:07:35 +0000

    logic lib

---
+++ logic/src/main/java/com/example/logic/StudentGame.kt
@@ -0,0 +1,40 @@
+package com.example.logic
+import com.example.lib.GameInterface
+
+class StudentGame (columns: Int = 7, rows: Int = 10) : GameInterface {
+
+
+    override val mColumns: Int = columns
+    override val mRows: Int = rows
+    // Set up two-dimensional array of integer, of size columns x rows
+    // and fill it with zeroes
+
+    override var mData: Array<IntArray> = Array(columns) { IntArray(rows, {0}) }
+
+    // Not specified in the interface - just to show that other things can be added
+    var playerTurn: Int = 1
+
+
+    // Returns the state of the game board at a specified column and row number
+
+    override fun getToken(column: Int, row: Int): Int {
+        return mData[column][row]     }
+
+
+    // Changes the contents of the game board at a specified row and column
+    override fun playToken(column: Int, player: Int): Boolean {
+        if (player <= 0) {
+            throw IllegalArgumentException("Player numbers start with 1")
+        }
+
+        for (row in 0 until mRows) {
+            if (mData[column][row] === 0) {
+                mData[column][row] = player
+                return true
+            }
+        }
+        return false
+        // illegal move     }
+    }
+
+}
